470e60786bc0d47122894e1c8ade8d05a617cff8
rey <167632343+reyx-00@users.noreply.github.com>
2024-08-26 03:21:28 +0900

    パスの文字が表示されるように変更

--- src/routes/photos.js
+++ src/routes/photos.js
@@ -6,4 +6,9 @@ app.get("/", (c) => {
   return c.text("Some photos");
 });
 
+app.get("/:title", (c) => {
+  const title = c.req.param("title");
+  return c.text(title);
+})
+
 module.exports = app;
